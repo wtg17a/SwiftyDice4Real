bafa7ae3be96592309e15623d6b893ecf0ff1017
Will Gibbs <wtg17a@acu.edu>
2020-10-13 08:20:42 -0500

    Initial Commit

---
+++ SwiftyDice4Real/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyDice4Real
+//
+//  Created by William Gibbs on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
