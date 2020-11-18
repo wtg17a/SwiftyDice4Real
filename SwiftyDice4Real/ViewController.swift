3378cb2cfb28a73609f99c747d14239759976282
Will Gibbs <wtg17a@acu.edu>
2020-11-18 13:51:13 -0600

    Commit 3 (All done!)

--- SwiftyDice4Real/ViewController.swift
+++ SwiftyDice4Real/ViewController.swift
@@ -13,33 +13,48 @@ class ViewController: UIViewController {
     @IBOutlet var diceImageView: UIImageView!
     @IBOutlet var criticalLabel: UILabel!
     
-    let rollSound = Bundle.main.path(forResource: "rolldice", ofType: "mp3")
-    var rollAudioPlayer = AVAudioPlayer()
-    let critSound = Bundle.main.path(forResource: "zfanfare", ofType: "mp3")
-    var critAudioPlayer = AVAudioPlayer()
-    let failSound = Bundle.main.path(forResource: "failwah", ofType: "mp3")
-    var failAudioPlayer = AVAudioPlayer()
-
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        do {
-            rollAudioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: rollSound!))
-        } catch {
-            print(error)
+    }
+    
+    func playSound(id: Int) {
+        
+        var soundName: String
+        
+        switch(id) {
+        case 1:
+            soundName = "rolldice"
+            break
+        case 2:
+            soundName = "zfanfare"
+            break
+        case 3:
+            soundName = "failwah"
+            break
+        default:
+            soundName = "torpedo"
+        }
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else {
+            print("Sound file not found.")
+            return
         }
         
         do {
-            critAudioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: critSound!))
-        } catch {
-            print(error)
-        }
-        
-        do {
-            failAudioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: failSound!))
-        } catch {
-            print(error)
-        }
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+                guard let audioPlayer = audioPlayer else { return }
+
+                audioPlayer.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
     }
 
     @IBAction func buttonPress() {
@@ -53,22 +68,26 @@ class ViewController: UIViewController {
     func rollDie() {
         let rolledNumber = Int.random(in: 1...20)
         var imageName = "d\(rolledNumber)"
-        
-        rollAudioPlayer.play()
+        var rollSoundId = 1
         
         if(rolledNumber == 7) {
             let roll2 = Int.random(in: 1...5)
             if(roll2 == 5) {
                 imageName = "d7cruiser"
+                rollSoundId = 7
             }
         }
+        
+        playSound(id: rollSoundId)
         
         diceImageView.image = UIImage(named: imageName)
         
         if(imageName == "d1") {
             criticalLabel.text = "Critical Failure"
+            playSound(id: 3)
         } else if(imageName == "d20") {
             criticalLabel.text = "Critical Success"
+            playSound(id: 2)
         } else {
             criticalLabel.text = ""
         }
